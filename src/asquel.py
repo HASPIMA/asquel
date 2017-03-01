054d81c37e52031de408ddd6b51bf308e9ba4100
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-03-01 10:26:23 -0700

    Updated interpreter
    Better look, better feel.

--- src/asquel.py
+++ src/asquel.py
@@ -1,27 +1,25 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import parser
-from math import *
+from xoloitzcuintle import *
 
-print 'Asquel versión 0.0.0.0c'
-print 'Copyright 2017 Alan Ramirez Zatarain'
-print 'Liberado al público bajo la BSD 3-Clause License'
-print 'Para aprender sobre la sintaxys de asquel ingresa a:'
-print 'http://asquel.hol.es/'
-print ' '
+clear()
+derechos()
 
+class Parsero(object):
+    PROMPT = chr(27)+"[1;32m"+"Asquelito$ "+chr(27)+"[0;37m"
+    ALERTA = "Error: La expresión está mal denotada.\n"
 
-def main():
-    while True:
-        try:
-            entrada = raw_input('asquelito$ ')
-        except EOFError:
-            break
-        if not entrada:
-            continue
+    def asquelito(self):
+        while True:
+            try:
+                resultado = evaluar(self.PROMPT)
+                print(chr(27)+"[1;37m""{}\n".format(resultado)+chr(27)+"[0;37m")
+            except EOFError:
+                print('')
+                break
+            except:
+                print(chr(27)+"[0;91m"+self.ALERTA+chr(27)+"[0;37m")
 
-	medio = parser.expr(entrada).compile()
-	resultado = eval(medio)
-	print '%s\n' % resultado
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    Parsero().asquelito()
