99758a4911cd1d6fa115dbfa61d62141b2326848
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-03-01 10:30:16 -0700

    Added build and clean scripts
    Compilar.sh = build
    Clean.sh = clean

---
+++ util/compilar.sh
@@ -0,0 +1,5 @@
+python -m py_compile ../src/asquel.py
+python -m py_compile ../src/xoloitzcuintle.py
+mv ../src/asquel.pyc ../bin/asquel.pyc
+mv ../src/xoloitzcuintle.pyc ../bin/xoloitzcuintle.pyc
+echo Asquel ha sido exitosamente construido
