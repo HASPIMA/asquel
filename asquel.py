bd628abf8781edad8c7d8ac908b780f2334a68a2
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-10-09 00:32:09 -0600

    Adaptado para nuevas caracteristicas
    Se modificó ligeramente para integrar el nuevo generador de arbol de proceso, junto con el mensaje de bienvenida.

--- asquel.py
+++ asquel.py
@@ -4,22 +4,26 @@
 # asquel.py - Interface de entrada y salida standard al usuario
 # El presente documento es parte del proyecto Asquel, cedido
 # a todo el mundo bajo la LICENCIA COMUN DE CODIGO ABIERTO ZATARAIN.
-# Puedes encontrar una copia en doc/LICENSE o en https://github.com/SoyZatarain/asquel/blob/master/doc/LICENSE
+# Puedes encontrar una copia en doc/LICENSE o en https://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE
 #
 # Copyright (c)2017 Alan Ramirez Zatarain.
 
 import sys # Esto es para poder obtener los parametros
-from asquetl import Diccionario # La clase que procesa todo
+from asquetl import * # Las clases que procesan todo
 
-def procesar(contenidoDelArchivo): # Esta funcion conecta el archivo de entrada con asquetl.Diccionario
-    return Diccionario().lex(contenidoDelArchivo) # Devuelve la matriz de resultados
+def procesar(contenidoDelArchivo): # Esta funcion conecta el archivo de entrada con asquetl.Diccionario y luego con asquetl.Analizador
+    matrizDeSimbolos = Diccionario().lex(contenidoDelArchivo) # Generar matriz de valores convertidos a simbolos y su tipo asignado
+    diagramaDeNiveles = Analizador().analizar(matrizDeSimbolos) # Generar niveles de jerarquia de resolucion para devolver un diagrama de arbol
+    return diagramaDeNiveles # Devuelve el arbol procesado con la jerarquia de niveles
 
 
 if __name__ == '__main__': # Si es ejecutado directamente
+    print("Asquel interpreter 0.2d\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n") #Avisos
     if len(sys.argv) == 2: # Si el usuario ingreso dos parametros
         if sys.argv[1].endswith(".asq", 0): # Si la extension de archivo es .ASQ 
-            print(procesar(open(sys.argv[1], "r").read())) # Imprimir lo que la funcion procesar() devuelva
+            salida = procesar(open(sys.argv[1], "r").read()) # Abrimos el archivo y lo convertimos en contenido apto para mandarlo a asquetl.Diccionario
+            print(salida) # Imprimir lo que la funcion procesar() devuelva
         else: # Si la extension del archivo no es .ASQ
             print(sys.argv[1], "no es un documento de Asquel") # Imprimir este aviso
     else: # Si el usuario ingreso uno o mas de 2 parametros
-        print("Asquel interpreter 0.2c\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n\nUso: ./asquel.py archivo.asq") #Avisos
+        print("Uso: [python3] asquel.py <archivo.asq>\n") #Avisos
