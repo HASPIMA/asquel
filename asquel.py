2ed840dbaee02d4fb029333a2b75c1f6c4a42f87
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-10-12 13:50:30 -0600

    Modificado para incluir Analizador.analizar()
    Dada su naturaleza modular, sólo es un paso adicional.

--- asquel.py
+++ asquel.py
@@ -10,15 +10,16 @@
 
 import sys # Esto es para poder obtener los parametros
 from asquetl import * # Las clases que procesan todo
+from asquetl.evaluador import Evaluador
 
 def procesar(contenidoDelArchivo): # Esta funcion conecta el archivo de entrada con asquetl.Diccionario y luego con asquetl.Analizador
     matrizDeSimbolos = Diccionario().lex(contenidoDelArchivo) # Generar matriz de valores convertidos a simbolos y su tipo asignado
-    diagramaDeNiveles = Analizador().analizar(matrizDeSimbolos) # Generar niveles de jerarquia de resolucion para devolver un diagrama de arbol
-    return diagramaDeNiveles # Devuelve el arbol procesado con la jerarquia de niveles
+    resultado = Evaluador().evaluar(Analizador().analizar(matrizDeSimbolos)) # Genera el arbol procesado con la jerarquia de niveles
+    return resultado # Devuelve todas las salidas, por ahora solo posibles con imprimir()
 
 
 if __name__ == '__main__': # Si es ejecutado directamente
-    print("Asquel interpreter 0.2d\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n") #Avisos
+    print("Asquel interpreter 0.99rc1\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n") #Avisos
     if len(sys.argv) == 2: # Si el usuario ingreso dos parametros
         if sys.argv[1].endswith(".asq", 0): # Si la extension de archivo es .ASQ 
             salida = procesar(open(sys.argv[1], "r").read()) # Abrimos el archivo y lo convertimos en contenido apto para mandarlo a asquetl.Diccionario
