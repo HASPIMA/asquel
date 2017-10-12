e7ce00c819348a33c61e6d66b5bef783cca9312c
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-10-12 13:47:48 -0600

    Evaluador completo
    Se agregaron funciones adicionales para pasos intermedios, se definieron las funciones anteriormente seleccionadas, se denominó el sistema de errores de sintaxis.

--- asquetl/evaluador.py
+++ asquetl/evaluador.py
@@ -1,29 +1,85 @@
 from asquetl import *
 import sys
 
-class Evaluador:                # Nuestro objeto
-    def __init__(object):       # El respectivo constructor
-        if object == None:      # En caso de que no recibamos el preprocesado
-            sys.exit()          # El programa abortará para que no se quede en un loop infinito
+class Evaluador:
+    def __init__(object):
+        if object == None:
+            sys.exit()
         pass
 
-    def evaluar():              # La función principal, lo que finalmente generará la salida, haciendo uso de las siguientes funciones secundarias:
-        pass
+    def evaluar(self, nivel):
+        self.variables = {}
+        self.resultadoParcial = ""
+        self.resolverNivel(nivel)
+        return self.resultadoParcial
 
-    def cargarPrograma():       # Separa al programa preprocesado en líneas que se pueden ejecutarse como pasos consecutivos
-        pass
+    def cargarPrograma(self, nivel):
+        for declaracion in nivel.modificadores:
+            declaracion.cargar(self)
 
-    def resolverNivel():        # Ir eliminando los niveles que ya estén en su forma final y lanzarlos al inferior
-        pass
+    def resolverNivel(self, nivel):
+        try:
+            return nivel.cargar(self)
+        except AttributeError:
+            raise ValueError("Error 003")
 
-    def primeraPrioridad():     # Resolver expresiones
-        pass
+    def primeraPrioridad(self, nivel):
+        try:
+            nivel1 = self.resolverNivel(nivel.primerNum)
+            nivel2 = self.resolverNivel(nivel.segundoNum)
+        except AttributeError:
+            raise ValueError("Error 004")
+        if nivel.valor == "*":
+            return nivel1 * nivel2
+        elif nivel.valor == "/":
+            return nivel1 / nivel2
+        else:
+            raise ValueError("Error 005")
 
-    def segundaPrioridad():     # Resolver expresiones
-        pass
+    def segundaPrioridad(self, nivel):
+        try:
+            nivel1 = self.resolverNivel(nivel.primerNum)
+            nivel2 = self.resolverNivel(nivel.segundoNum)
+        except AttributeError:
+            raise ValueError("Error 004")
+        if nivel.valor == "+":
+            return nivel1 + nivel2
+        elif nivel.valor == "-":
+            return nivel1 - nivel2
+        else:
+            raise ValueError("Error 006")
 
-    def cargarNivelAnidado():   # Un nivel dentro de otro nivel por ejemplo imprimir(1+2/7-8);
-        pass
+    def cargarNivelAnidado(self, nivel):
+        if not isinstance(nivel.primerNum, puntualizarEslabon):
+            raise ValueError("Error 008")
+        try:
+            nivel1 = nivel.primerNum.valor
+            nivel2 = self.resolverNivel(nivel.segundoNum)
+        except AttributeError:
+            raise ValueError("Error 004")
+        self.variables[nivel1] = nivel2
 
-    def funcion():              # Funciones declaradas como imprimir()
-        pass
+    def funcion(self, nivel):
+        if not isinstance(nivel.primerNum, puntualizarEslabon):
+            raise ValueError("Error 009")
+        try:
+            nombreDeLaFuncion = nivel.primerNum.valor
+            modificador = self.resolverNivel(nivel.segundoNum)
+        except AttributeError:
+            raise ValueError("Error 004")
+        if nombreDeLaFuncion == "imprimir":
+            self.resultadoParcial += str(modificador) + "\n"
+        else:
+            raise ValueError("Error 010")
+
+    def cargarPorIndice(self, nivel):
+        try:
+            return self.variables[nivel.valor]
+        except KeyError:
+            raise ValueError("Error 005")
+
+    def cargarPorValor(self, nivel):
+        try:
+            return int(nivel.valor)
+        except ValueError:
+            raise ValueError("Error 006")
