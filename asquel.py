86382d7eecda35134e9e41a99a45e3fd496c3acc
Jesus Alan Ramirez Zatarain <jarztv@gmail.com>
2017-11-10 16:10:38 -0600

    Opciones de consola
    Agregadas opciones para manipular a voluntad los datos que arroje cada modulo.

--- asquel.py
+++ asquel.py
@@ -11,20 +11,35 @@
 import sys # Esto es para poder obtener los parametros
 from asquetl import * # Las clases que procesan todo
 
-def procesar(contenidoDelArchivo): # Esta funcion conecta el archivo de entrada con asquetl.Diccionario y luego con asquetl.Analizador
+def procesar(contenidoDelArchivo): # Esta funcion conecta el archivo de entrada con asquetl.*
     matrizDeSimbolos = Diccionario().lex(contenidoDelArchivo) # Generar matriz de valores convertidos a simbolos y su tipo asignado
     diagramaDeNiveles = Analizador().analizar(matrizDeSimbolos) # Generar niveles de jerarquia de resolucion para devolver un diagrama de arbol
-    resultado = Evaluador().evaluar(diagramaDeNiveles) # Genera el arbol procesado con la jerarquia de niveles
+    resultado = Evaluador().evaluar(diagramaDeNiveles) # Resuelve los niveles uno por uno hasta llegar al fondo
     return resultado # Devuelve todas las salidas, por ahora solo posibles con imprimir()
 
-
 if __name__ == '__main__': # Si es ejecutado directamente
-    print("Asquel interpreter 0.99rc2\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n") #Avisos
     if len(sys.argv) == 2: # Si el usuario ingreso dos parametros
-        if sys.argv[1].endswith(".asq", 0): # Si la extension de archivo es .ASQ 
+        if sys.argv[1].endswith(".asq", 0): # Si la extension de archivo es .ASQ
             salida = procesar(open(sys.argv[1], "r").read()) # Abrimos el archivo y lo convertimos en contenido apto para mandarlo a asquetl.Diccionario
             print(salida) # Imprimir lo que la funcion procesar() devuelva
         else: # Si la extension del archivo no es .ASQ
             print(sys.argv[1], "no es un documento de Asquel") # Imprimir este aviso
+    elif len(sys.argv) == 3:
+        opcion = sys.argv[1]
+        archivo = open(sys.argv[2], "r").read()
+        if opcion == "-d" or opcion == "--diccionario":
+            print(Diccionario().lex(archivo))
+        elif opcion == "-a" or opcion == "--arbol":
+            print(Analizador().analizar(Diccionario().lex(archivo)))
+        else:
+            print("Opcion invalida: %s" % opcion)
     else: # Si el usuario ingreso uno o mas de 2 parametros
-        print("Uso: [python3] asquel.py <archivo.asq>\n") #Avisos
+        print("Asquel interpreter 1.0\nCopyright 2017 Alan Ramirez Zatarain\nhttps://raw.githubusercontent.com/SoyZatarain/asquel/master/doc/LICENSE\n") #Avisos
+        if sys.argv[0] == "asquel.py" or sys.argv[0] == "./asquel.py":
+            programa = "python3 asquel.py"
+        else:
+            programa = "asquel"
+        print("Uso: %s [opcion] <archivo.asq>\n" % programa) #Avisos
+        print("  Opciones:")
+        print("  -d, --diccionario      Muestra asignación de tipo a los simbolos")
+        print("  -a, --arbol            Muestra arbol de sintaxis abstracta")
